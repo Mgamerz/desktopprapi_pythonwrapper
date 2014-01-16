75b82b2789d787ff9050d9b88f15b1cf11083807
Mgamerz <developer.mgamerzproductions@gmail.com>
2014-01-16 09:08:58 -0700

    Added new test.

--- Desktoppr API Wrapper/DesktopprTester.py
+++ Desktoppr API Wrapper/DesktopprTester.py
@@ -16,6 +16,13 @@ class Test(unittest.TestCase):
         self.assertEqual(api.sync_wallpaper(202),None)
         self.assertEqual(api.unsync_wallpaper(203),None)
         
+        
+    def testLikes(self):
+        api=DesktopprAPI()
+        
+    def testSyncStatus(self):
+        api = DesktopprAPI()
+        self.assertTrue(api.check_if_synced('mgamerz',18028))
     #More tests to be written.
 
 
